8d0d053f37cc7f10d1eeb671bab8ba8d696471b4
Milesloh <43357648+Milesloh@users.noreply.github.com>
2018-09-28 10:20:06 -0500

    AudioTest

---
+++ AudioTest/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1538145362,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"media/a1.m4a",
+		"media/a1.ogg",
+		"media/b1.m4a",
+		"media/b1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
