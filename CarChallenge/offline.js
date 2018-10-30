dfc739f6346d1a1fe32b88e4130119c3d75e30fe
Milesloh <43357648+Milesloh@users.noreply.github.com>
2018-10-30 09:31:44 -0500

    Added Car Challenege

---
+++ CarChallenge/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1540909626,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/flag-sheet0.png",
+		"images/line-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
