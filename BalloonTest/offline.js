0a1423408322dd6514adb8139202efc9d63a9049
Milesloh <43357648+Milesloh@users.noreply.github.com>
2018-11-07 15:49:39 -0600

    Added Balloon Test

---
+++ BalloonTest/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1541627244,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/circlepoint-sheet0.png",
+		"images/circleline-sheet0.png",
+		"images/particle-sheet0.png",
+		"images/balloon-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
