668334ed00dc86dfcfe469ad4934bb628b0c909c
Milesloh <43357648+Milesloh@users.noreply.github.com>
2018-11-24 14:02:21 -0600

    DiaDau's Task

---
+++ DiaDausTask/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1543089668,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/barrier-sheet0.png",
+		"images/moveable-sheet0.png",
+		"media/pluck.m4a",
+		"media/pluck.ogg",
+		"media/pluck2.m4a",
+		"media/pluck2.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
