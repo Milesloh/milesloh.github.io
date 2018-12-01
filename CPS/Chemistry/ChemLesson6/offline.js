e38f8c64d1fc8b0694b98a9c2e4eaf65868331d8
Milesloh <43357648+Milesloh@users.noreply.github.com>
2018-12-01 08:33:29 -0600

    fixed bug: balloon shrinking while paused

--- CPS/Chemistry/ChemLesson6/offline.js
+++ CPS/Chemistry/ChemLesson6/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1543594353,
+	"version": 1543674756,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
--- CPS/Chemistry/ChemLesson9/offline.js
+++ CPS/Chemistry/ChemLesson9/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1543594577,
+	"version": 1543674351,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
