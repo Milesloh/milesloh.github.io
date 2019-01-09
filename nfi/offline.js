7abd6983acfb8b4dba46c70c9f930f52bfefb640
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-01-09 08:08:20 -0600

    nfi Newtons First Isolator

---
+++ nfi/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1547042787,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/truck-sheet0.png",
+		"images/truck2-sheet0.png",
+		"images/bike-sheet0.png",
+		"images/cycler-sheet0.png",
+		"images/varrowstem-sheet0.png",
+		"images/ahead-sheet0.png",
+		"images/vhead-sheet0.png",
+		"images/ayhead-sheet0.png",
+		"images/vyhead-sheet0.png",
+		"images/ball2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
