2313103378bd56e3cc78279347cc8ce6d9c9799a
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-01-11 08:33:12 -0600

    one more text fix attempt

--- sip/offline.js
+++ sip/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1547216818,
+	"version": 1547217121,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -15,6 +15,10 @@
 		"icon-114.png",
 		"icon-128.png",
 		"icon-256.png",
-		"loading-logo.png"
+		"loading-logo.png",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff"
 	]
 }
