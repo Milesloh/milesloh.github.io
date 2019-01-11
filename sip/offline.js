fe9445d0a80d7b86a594e07a334d3a8fe443ddfa
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-01-11 08:13:27 -0600

    sip S1 Practice

---
+++ sip/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1547215911,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/physobjtemplate-sheet0.png",
+		"images/ruler-sheet0.png",
+		"images/ruler-sheet1.png",
+		"images/numberenternumberback-sheet0.png",
+		"images/numberenternumberfieldback-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
