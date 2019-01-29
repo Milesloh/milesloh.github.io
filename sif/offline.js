fe02782192b8311fa16ee1eab967128741eb3a22
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-01-29 08:49:13 -0600

    sif semester1 final

---
+++ sif/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1548773253,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/physobjtemplate-sheet0.png",
+		"images/numberenternumberfieldback-sheet0.png",
+		"images/numberenternumberfieldback-sheet1.png",
+		"images/ruler-sheet0.png",
+		"images/ruler-sheet1.png",
+		"images/hiddenbutton-sheet0.png",
+		"images/buttontarget-sheet0.png",
+		"images/waveparticletemplate-sheet0.png",
+		"images/waveline-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"stylesheet.css",
+		"parse-1.6.14.min.js"
+	]
+}
