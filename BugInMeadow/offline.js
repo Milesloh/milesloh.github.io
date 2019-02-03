7ecc16af0f7d9030de93d6a38bfbfed7c8ec15b5
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-02-03 10:30:24 -0600

    Bug In Meadow

---
+++ BugInMeadow/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1549210440,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/meadow-sheet0.png",
+		"images/meadow-sheet1.png",
+		"images/bug-sheet0.png",
+		"images/placearea-sheet0.png",
+		"images/finishedcover-sheet0.png",
+		"images/blackbox-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
