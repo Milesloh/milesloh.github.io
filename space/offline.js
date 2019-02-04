37a38348d00b7f0982b42180bb6e7dc8ca35ccf7
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-02-04 09:57:44 -0600

    space

---
+++ space/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1549295722,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ship-sheet0.png",
+		"images/ship-sheet1.png",
+		"images/star-sheet0.png",
+		"images/asteroid-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
