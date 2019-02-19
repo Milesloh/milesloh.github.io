f338039f739b8700bf51adca869da4ccc8ccdd1f
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-02-19 13:25:16 -0600

    ShipGame

---
+++ ShipGame/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1550604262,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/wall-sheet0.png",
+		"images/wall-sheet1.png",
+		"images/ship-sheet0.png",
+		"images/jet-sheet0.png",
+		"images/codeback-sheet0.png",
+		"images/star-sheet0.png",
+		"images/spot-sheet0.png",
+		"images/space-sheet0.png",
+		"images/target-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
