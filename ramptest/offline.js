ed2849ebf8a925997051645f64262962e579bbbc
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-03-29 10:42:10 -0500

    ramp test for 25 week retake

---
+++ ramptest/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1553872534,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/ground-sheet0.png",
+		"images/ramp-sheet0.png",
+		"images/tophandle-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/invisiblebutton1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
