88afb2400f52f63af41d4aa31546d077b8c97c13
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-04-22 23:07:55 -0500

    citscibats Citizen Science Bats

---
+++ citscibats/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1555992352,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/button-sheet0.png",
+		"images/button-sheet1.png",
+		"images/picture-sheet0.png",
+		"images/picture-sheet1.png",
+		"images/picture-sheet2.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"parse-1.6.14.min.js"
+	]
+}
