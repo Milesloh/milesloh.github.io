891fad1d1829f1b815881160c81b763a4fc64dbd
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-05-03 08:38:35 -0500

    enid update energy identification

---
+++ enid/offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1556890646,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/helicopter-sheet0.png",
+		"images/helicopter-sheet1.png",
+		"images/ground-sheet0.png",
+		"images/sky-sheet0.png",
+		"images/hiddenbutton4-sheet0.png",
+		"images/helpback-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff"
+	]
+}
