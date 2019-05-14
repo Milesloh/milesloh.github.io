3b8e211bd756cc858f13622d2b335f9e5c7e448e
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-05-13 19:10:08 -0500

    formike bat joke app

---
+++ formike/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1557792544,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/button-sheet0.png",
+		"images/button-sheet1.png",
+		"images/picture-sheet0.png",
+		"images/picture-sheet1.png",
+		"images/picture-sheet2.png",
+		"images/pic-sheet0.png",
+		"images/pic2-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"parse-1.6.14.min.js"
+	]
+}
