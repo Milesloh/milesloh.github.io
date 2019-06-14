a72da81cfd93b040f21385e29088630db284856c
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-06-14 08:24:41 -0500

    gix Grade IX Name Picker

---
+++ gix/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1560518583,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/physobjtemplate-sheet0.png",
+		"images/numberenternumberfieldback-sheet0.png",
+		"images/numberenternumberfieldback-sheet1.png",
+		"images/hiddenbutton-sheet0.png",
+		"images/buttontarget-sheet0.png",
+		"images/waveparticletemplate-sheet0.png",
+		"images/waveparticletemplate-sheet1.png",
+		"images/waveline-sheet0.png",
+		"images/graphback-sheet0.png",
+		"images/ink-sheet0.png",
+		"images/lohslider-sheet0.png",
+		"images/sliderfill-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"stylesheet.css"
+	]
+}
