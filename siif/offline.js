d7fd90cb99e1e09beedce0e56f2032b830a50bdb
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-06-16 22:16:02 -0500

    siif 0.1.0.0 SII Final

---
+++ siif/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1560741272,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/helicopter-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/wave.png",
+		"images/sky-sheet0.png",
+		"images/resetbutton-sheet0.png",
+		"images/overlaybutton1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff",
+		"parse-1.6.14.min.js"
+	]
+}
