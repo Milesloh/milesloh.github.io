aadf129c8399d6fb92ad1f4d0115d21f4c016a98
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-09-06 08:08:05 -0500

    air 0.0.1.0
    Simulation of air molecules hitting a newspaper

---
+++ air/offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1567775131,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/newspaper-sheet0.png",
+		"images/table-sheet0.png",
+		"images/air-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
