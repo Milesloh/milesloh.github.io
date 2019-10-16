bf0e75dca38bf8e4fbd4121f7aa0e0357048f0f1
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-10-15 21:40:54 -0500

    CPS Unit2Lesson3

---
+++ CPS/Chemistry/Unit2Lesson3/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1571188974,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/wall-sheet0.png",
+		"images/iron-sheet0.png",
+		"images/water-sheet0.png",
+		"images/airboundry-sheet0.png",
+		"images/oxygen-sheet0.png",
+		"images/rust-sheet0.png",
+		"images/saleen-sheet0.png",
+		"images/menuback-sheet0.png",
+		"images/watergenarea-sheet0.png",
+		"images/sliderfill-sheet0.png",
+		"images/slider-sheet0.png",
+		"images/oil-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff"
+	]
+}
