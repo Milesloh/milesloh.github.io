8a71b26ac548724ea373069fade5da29c3282fa8
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-11-19 07:32:20 -0600

    skydi 0.1.0.0 Sky Diver simulation without spedometer

---
+++ skydi/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1574169676,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/cloud-sheet0.png",
+		"images/airplane-sheet0.png",
+		"images/target-sheet0.png",
+		"images/spedometer-sheet0.png",
+		"images/spedometertick-sheet0.png",
+		"images/hidbut1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
