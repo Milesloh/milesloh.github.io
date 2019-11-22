92da728230e4a3b1f22693e22228d9d2753f1ff4
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-11-22 07:46:35 -0600

    grav 0.1.0.0 Stopwatch for Measuring Gravity with procedure

---
+++ grav/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1574430326,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/dot-sheet0.png",
+		"images/ink-sheet0.png",
+		"images/slider-sheet0.png",
+		"images/sliderfill-sheet0.png",
+		"images/buttonback-sheet0.png",
+		"images/buttonback-sheet1.png",
+		"images/buttonback-sheet2.png",
+		"images/origin-sheet0.png",
+		"images/tiletarget-sheet0.png",
+		"images/hiddenbutton1-sheet0.png",
+		"images/numberfieldback-sheet0.png",
+		"images/numberfieldback-sheet1.png",
+		"images/helppicture-sheet0.png",
+		"images/helppicture-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
