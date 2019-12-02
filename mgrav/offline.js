d2ffbade502bfc1b59906a79f3591fc5432da437
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-12-02 07:40:58 -0600

    grav - 0.1.0.0 simulation to measure gravity.

---
+++ mgrav/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1575293954,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/boulder-sheet0.png",
+		"images/greyback-sheet0.png",
+		"images/selectionbox-sheet0.png",
+		"images/steven-sheet0.png",
+		"images/line-sheet0.png",
+		"images/ruler-sheet0.png",
+		"images/hbutton1-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
