4a2521b7443aec3a3faa1c448dd3231e7230b3c2
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-12-05 08:02:55 -0600

    wxvr - 0.1.0.2 Moons of Jupiter for Week 15 Retake

---
+++ wxvr/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1575554342,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/greyback-sheet0.png",
+		"images/selectionbox-sheet0.png",
+		"images/line-sheet0.png",
+		"images/ruler-sheet0.png",
+		"images/hbutton1-sheet0.png",
+		"images/building-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
