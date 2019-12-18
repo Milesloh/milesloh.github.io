fcc6c29dd81e36a6100b3f2630c758a41329d172
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-12-17 20:50:07 -0600

    wht - 0.0.2.2 - linked to launcher

--- wht/offline.js
+++ wht/offline.js
@@ -1,9 +1,9 @@
 ﻿{
-	"version": 1550002332,
+	"version": 1576637226,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
-		"jquery-2.1.1.min.js",
+		"jquery-3.4.1.min.js",
 		"offlineClient.js",
 		"images/physobjtemplate-sheet0.png",
 		"images/numberenternumberfieldback-sheet0.png",
