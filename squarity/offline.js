78018c1a2393789a5d6512bb528ec45a8de96472
Milesloh <43357648+Milesloh@users.noreply.github.com>
2019-12-24 07:25:09 -0600

    squarity 0.0.4.0

---
+++ squarity/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1577193648,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/square-sheet0.png",
+		"images/fakesquare-sheet0.png",
+		"images/darkeningmask-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"stylesheet.css",
+		"weblysleekuil-webfont.eot",
+		"weblysleekuil-webfont.svg",
+		"weblysleekuil-webfont.ttf",
+		"weblysleekuil-webfont.woff"
+	]
+}
