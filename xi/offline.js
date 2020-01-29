e6c01e313ce55fa9ba545c3391ac2b41ae85ebae
Milesloh <43357648+Milesloh@users.noreply.github.com>
2020-01-29 08:11:24 -0600

    xi 0.0.1.0 semester 1 final exam 2020

---
+++ xi/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1580306870,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/ball-sheet0.png",
+		"images/ground-sheet0.png",
+		"images/boulder-sheet0.png",
+		"images/greyback-sheet0.png",
+		"images/selectionbox-sheet0.png",
+		"images/lens-sheet0.png",
+		"images/hbutton3-sheet0.png",
+		"images/ruler-sheet0.png",
+		"images/ruler-sheet1.png",
+		"images/wall-sheet0.png",
+		"media/chime.m4a",
+		"media/chime.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
