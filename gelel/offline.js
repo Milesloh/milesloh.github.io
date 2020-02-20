4a162086f8f1778bde9ca09ac844f6a2cf7c0ecf
Milesloh <43357648+Milesloh@users.noreply.github.com>
2020-02-19 22:08:51 -0600

    gelel gel electro simulation

---
+++ gelel/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1582171658,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/gelback-sheet0.png",
+		"images/nucleotide-sheet0.png",
+		"images/startpoint-sheet0.png",
+		"images/chalkbutton-sheet0.png",
+		"images/eraserbutton-sheet0.png",
+		"images/eraser-sheet0.png",
+		"images/chalk-sheet0.png",
+		"images/cursor-sheet0.png",
+		"images/blackboard-sheet0.png",
+		"images/fragment-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
