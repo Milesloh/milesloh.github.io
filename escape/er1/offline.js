1d06b4a2af9602ae703735d5a47517369cbd7e1f
Milesloh <43357648+Milesloh@users.noreply.github.com>
2020-03-17 17:06:49 -0500

    escape room apps

---
+++ escape/er1/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1584482586,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bacll-sheet0.png",
+		"images/bacll-sheet1.png",
+		"images/bacll-sheet2.png",
+		"images/wall-sheet0.png",
+		"images/changeregion-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ escape/er3/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1584482724,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/up-sheet0.png",
+		"images/down-sheet0.png",
+		"images/lock-sheet0.png",
+		"images/confetti-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
